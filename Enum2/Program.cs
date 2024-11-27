6faa7458f589b9bbfa5c5e2444b8d32e25b5916d
Emine Naz Duran <147312294+eminenazduran@users.noreply.github.com>
2024-11-27 21:03:18 +0300

    Add files via upload

---
+++ Enum2/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Enum2
+{
+    using System;
+
+    enum TrafficLight
+    {
+        Red = 1,
+        Yellow,
+        Green
+    }
+
+    class TrafficControl
+    {
+        public static void ActionBasedOnTrafficLight(TrafficLight light)
+        {
+            switch (light)
+            {
+                case TrafficLight.Red:
+                    Console.WriteLine("Stop! The light is red.");
+                    break;
+                case TrafficLight.Yellow:
+                    Console.WriteLine("Get ready! The light is yellow.");
+                    break;
+                case TrafficLight.Green:
+                    Console.WriteLine("Go! The light is green.");
+                    break;
+                default:
+                    Console.WriteLine("Invalid traffic light status.");
+                    break;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            TrafficLight currentLight = TrafficLight.Green;
+            TrafficControl.ActionBasedOnTrafficLight(currentLight); // Output: Go! The light is green.
+        }
+    }
+
+}
---
+++ Enum3/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Enum3
+{
+    using System;
+
+    enum EmployeeRole
+    {
+        Manager,
+        Developer,
+        Designer,
+        Tester
+    }
+
+    class SalaryCalculator
+    {
+        public static void CalculateSalary(EmployeeRole role)
+        {
+            decimal salary = 0;
+
+            switch (role)
+            {
+                case EmployeeRole.Manager:
+                    salary = 8000;
+                    break;
+                case EmployeeRole.Developer:
+                    salary = 6000;
+                    break;
+                case EmployeeRole.Designer:
+                    salary = 5000;
+                    break;
+                case EmployeeRole.Tester:
+                    salary = 4000;
+                    break;
+                default:
+                    Console.WriteLine("Invalid role.");
+                    return;
+            }
+
+            Console.WriteLine($"The salary for the {role} is: ${salary}");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            EmployeeRole employeeRole = EmployeeRole.Developer;
+            SalaryCalculator.CalculateSalary(employeeRole); // Output: The salary for the Developer is: $6000
+        }
+    }
+
+}
